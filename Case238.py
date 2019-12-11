d7e72ebf60860263d1d7f596a634f861b7d8bd16
guxiajun <guxiajun@agora.io>
2019-12-11 16:27:16 +0800

    add testcases

---
+++ Case238.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ctypes
+import time
+import os
+import random
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+path = BASE_DIR + "/libpycmd.so"
+print(path)
+
+
+def needterms():
+    return "3"
+def categories():
+    return "broadcast"
+def shortDesc():
+    return "(外部源直播)进入频道，三个主播连麦360P800K,限制下行500K20%丢包，统计设备B侧的卡顿率和延时"
+def detailDesc():
+    return "(直播)A B C设备一直在同一个频道内，设置设备的上行丢包策略，通过log计算播放侧的卡顿率"
+
+
+def run():
+    ll = ctypes.cdll.LoadLibrary
+    lib = ll(path)
+    lib.ExeCmdCallBack(0, "impairNet,0")
+    lib.ExeCmdCallBack(1, "impairNet,0")
+    lib.ExeCmdCallBack(2, "impairNet,0")
+
+    lib.ExeCmdCallBack(0, "setParameters,{\"rtc.log_filter\":32847}")
+    lib.ExeCmdCallBack(1, "setParameters,{\"rtc.log_filter\":32847}")
+    lib.ExeCmdCallBack(2, "setParameters,{\"rtc.log_filter\":32847}")
+
+    lib.ExeCmdCallBack(0, "setParameters,{\"rtc.log_size\":20000000}")
+    lib.ExeCmdCallBack(1, "setParameters,{\"rtc.log_size\":20000000}")
+    lib.ExeCmdCallBack(2, "setParameters,{\"rtc.log_size\":20000000}")
+
+    lib.ExeCmdCallBack(0, "setParameters,{\"che.video.LogcatVideoQoS\":1}")
+    lib.ExeCmdCallBack(1, "setParameters,{\"che.video.LogcatVideoQoS\":1}")
+    lib.ExeCmdCallBack(2, "setParameters,{\"che.video.LogcatVideoQoS\":1}")
+
+    lib.ExeCmdCallBack(0, "setChannelProfile,1")
+    lib.ExeCmdCallBack(0, "setClientRole,1,nil")
+    lib.ExeCmdCallBack(0, "setVideoEncoderConfiguration,640,360,15,800,0")
+    lib.ExeCmdCallBack(0, "enableVideo")
+    #lib.ExeCmdCallBack(0, "switchCamera")
+    lib.ExeCmdCallBack(0, "setupLocalVideo,2,-1")
+    lib.ExeCmdCallBack(0, "setupRemoteVideo,2,2,-1")
+    lib.ExeCmdCallBack(0, "setupRemoteVideo,3,2,-1")
+    Testchannelname = "Test" + str(random.random())
+    lib.ExeCmdCallBack(0, "joinChannelByKey,nil," + Testchannelname + ",nil,1")
+
+    lib.ExeCmdCallBack(1, "setChannelProfile,1")
+    lib.ExeCmdCallBack(1, "setClientRole,1,nil")
+    lib.ExeCmdCallBack(1, "setVideoEncoderConfiguration,640,360,15,800,0")
+    lib.ExeCmdCallBack(1, "enableVideo")
+    #lib.ExeCmdCallBack(1, "switchCamera")
+    lib.ExeCmdCallBack(1, "setupLocalVideo,2,-1")
+    lib.ExeCmdCallBack(1, "setupRemoteVideo,1,2,-1")
+    lib.ExeCmdCallBack(1, "setupRemoteVideo,3,2,-1")
+    lib.ExeCmdCallBack(1, "joinChannelByKey,nil," + Testchannelname + ",nil,2")
+
+    lib.ExeCmdCallBack(2, "setChannelProfile,1")
+    lib.ExeCmdCallBack(2, "setClientRole,1,nil")
+    lib.ExeCmdCallBack(2, "setVideoEncoderConfiguration,640,360,15,800,0")
+    lib.ExeCmdCallBack(2, "enableVideo")
+    #lib.ExeCmdCallBack(2, "switchCamera")
+    lib.ExeCmdCallBack(2, "setupLocalVideo,2,-1")
+    lib.ExeCmdCallBack(2, "setupRemoteVideo,1,2,-1")
+    lib.ExeCmdCallBack(2, "setupRemoteVideo,2,2,-1")
+    lib.ExeCmdCallBack(2, "joinChannelByKey,nil," + Testchannelname + ",nil,3")
+
+    time.sleep(10)
+    lib.ExeCmdCallBack(1, "impairNet,0 0 0 500 20")
+
+    lib.ExeCmdCallBack(0, "SLEEP,180")
+
+    lib.ExeCmdCallBack(0, "leaveChannel")
+    lib.ExeCmdCallBack(1, "leaveChannel")
+    lib.ExeCmdCallBack(2, "leaveChannel")
+
+    lib.ExeCmdCallBack(1, "impairNet,0")
+
+    lib.ExeCmdCallBack(0, "getFile")
+    lib.ExeCmdCallBack(1, "getFile")
+    lib.ExeCmdCallBack(2, "getFile")
+
+    lib.ExeCmdCallBack(1, "DELAY")
+
+    return "4"
+
+
+
